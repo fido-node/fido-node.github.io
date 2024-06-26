2ccb41170223c73ca7941e730f47c18c28c02634
fido-node <fido-node@users.noreply.github.com>
2024-06-26 04:55:58 +0000

    deploy: fido-node/org@ac7df70e9b19ca3a97bddebda8b925a5c7054f46

--- resources/js/theme-selector.js
+++ resources/js/theme-selector.js
@@ -1,24 +1,26 @@
+hljs.highlightAll(); // Initialize highlight.js
+
 // Function to set the theme based on the preferred color scheme
-function setPrismTheme() {
-  const prismThemeLink = document.getElementById("prism-theme");
+function setHighlightTheme() {
+  const highlightThemeLink = document.getElementById("highlight-theme");
   const darkTheme =
-    "https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/themes/prism-tomorrow.min.css";
+    "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/base16/solarized-dark.min.css";
   const lightTheme =
-    "https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/themes/prism-solarizedlight.min.css";
+    "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/base16/solarized-light.min.css";
   if (
     window.matchMedia &&
     window.matchMedia("(prefers-color-scheme: dark)").matches
   ) {
-    prismThemeLink.href = darkTheme;
+    highlightThemeLink.href = darkTheme;
   } else {
-    prismThemeLink.href = lightTheme;
+    highlightThemeLink.href = lightTheme;
   }
 }
 
 // Initial theme set
-setPrismTheme();
+setHighlightTheme();
 
 // Listen for changes in the preferred color scheme
 window
   .matchMedia("(prefers-color-scheme: dark)")
-  .addEventListener("change", setPrismTheme);
+  .addEventListener("change", setHighlightTheme);
